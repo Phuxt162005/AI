e14720b0cbb1794c13e8c7fe80412f5604701616
Phuxt162005 <phuxt162005@gmail.com>
2026-09-08 10:23:19 +0700

    sửa 3.1 tiếp

--- core/exceptions.py
+++ core/exceptions.py
@@ -4,6 +4,16 @@
 class ProjectAIError(Exception):
     """Base exception for ProjectAI."""
 
-
 class ConfigurationError(ProjectAIError):
     """Raised when application configuration is invalid."""
+
+class ProjectAIError(Exception):
+    """Base exception for the project."""
+
+
+class ConfigurationError(ProjectAIError):
+    """Raised when configuration is invalid."""
+
+
+class InterfaceError(ProjectAIError):
+    """Raised when a component violates an interface contract."""
