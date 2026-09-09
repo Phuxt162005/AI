95e21ec06a3e3b3f2b40f15e58814ecf5a65aeb4
Phuxt162005 <phuxt162005@gmail.com>
2026-09-09 08:21:18 +0700

    sửa 3.2.4

--- self_built/__init__.py
+++ self_built/__init__.py
@@ -1,4 +1,5 @@
 """Self-built components for ProjectAI."""
 
 from .losses import BinaryCrossEntropyLoss, Loss, MSELoss
+from .model import Model, TrainingHistory
 from .optimizers import Optimizer, SGD
