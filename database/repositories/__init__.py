8313e2a65680b087822386533f973c53853c8f67
Phuxt162005 <phuxt162005@gmail.com>
2026-09-21 19:34:12 +0700

    sửa II.G

--- database/repositories/__init__.py
+++ database/repositories/__init__.py
@@ -1,9 +1,15 @@
-from .user_repository import UserRepository
+from .user_repository import (
+    UserRepository,
+    UserProfileRepository,
+    UserPreferenceRepository,
+)
 from .conversation_repository import ConversationRepository
 from .message_repository import MessageRepository
 
 __all__ = [
     "UserRepository",
+    "UserProfileRepository",
+    "UserPreferenceRepository",
     "ConversationRepository",
     "MessageRepository",
 ]
